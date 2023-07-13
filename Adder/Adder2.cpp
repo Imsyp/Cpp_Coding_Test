d6a760d99cc307ede0e4ff221b6e9778fe37510d
Imsyp <tkddud386@gmail.com>
2023-07-13 22:10:54 +0900

    20230713

---
+++ Adder/Adder2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    printf("%d + %d = %d",a,b,a+b);
+    return 0;
+}
