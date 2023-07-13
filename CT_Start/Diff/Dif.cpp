f05adcc9a247404f8e0267cbb7f5ae1755630b19
Imsyp <tkddud386@gmail.com>
2023-07-13 23:42:42 +0900

    20230713

---
+++ CT_Start/Diff/Dif.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) 
+{
+    int answer ;
+    answer = num1 - num2;
+    return answer;
+}
+
+int main(void)
+{
+    return 0;
+}
