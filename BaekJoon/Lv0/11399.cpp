fb172124f26fe04e7a7bec8730a0662b5479e7b3
Imsyp <tkddud386@gmail.com>
2023-12-18 18:45:57 +0900

    20231218

--- BaekJoon/Lv0/11399.cpp
+++ BaekJoon/Lv0/11399.cpp
@@ -1,4 +1,36 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+int main(void) {
+    int N;
+    int sum = 0;
+    int answer = 0;
+    cin >> N;
+
+    vector<int> times;
+
+    while(N--) {
+        int time;
+        cin >> time;
+        times.push_back(time);
+    }
+
+    sort(times.begin(), times.end());
+
+    for(int i = 0; i < times.size(); i++) {
+        if(i == 0) {
+            sum = times[0];
+        }
+        else {
+            sum = sum + times[i];
+        }
+        answer += sum;
+    }
+
+    cout << answer << endl;
+
+    return 0;
+}
